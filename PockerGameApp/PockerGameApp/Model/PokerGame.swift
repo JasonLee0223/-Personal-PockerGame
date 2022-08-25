ee0828851794430237d88e15f3f2b6274cacfcd0
Jason <ggj00125@gmail.com>
2022-08-26 04:28:49 +0900

    Feat: PokerGame 객체 구현

--- PockerGameApp/PockerGameApp/Model/PokerGame.swift
+++ PockerGameApp/PockerGameApp/Model/PokerGame.swift
@@ -7,49 +7,132 @@
 
 import Foundation
 
-class PockerGame {
+class PokerGame {
     
-    private(set) var dealer: Dealer
-    private(set) var players: participant
-    private(set) var realPlayers: [String]
-    private(set) var stud: Stud
+    var action = Action()
+    var state = State()
+    private var pokerPlayers = PokerPlayers()
+    private var dealer = Dealer()
     
-    init(stud: Stud) {
-        self.dealer = Dealer()
-        self.players = participant()
-        self.realPlayers = [String]()
-        self.stud = stud
-    }
+    private var pokerStud = Constants.defaultPockerStud
+    private var playerCount = PokerPlayers.Constants.defaultCount
     
-    func joinDealer() -> [String] {
+    init() {
+        action.pokerPlay = play
+        action.pokerReset = { self.resetGame() }
         
-        for index in 0..<players.members {
-            let tempMember = players.memberName[index]
-            realPlayers.append(tempMember)
+        action.inputPlayerCount = { playerCount in
+            self.playerCount = playerCount
+            self.state.resetPockerBoard(self.pokerStud, self.pokerPlayers.names)
         }
-        realPlayers.append(dealer.name)
-        return realPlayers
-    }
-    
-    func playStart() {
-        if dealer.isPossiblePlay(count: stud.rawValue * players.members) {
-            // 5카드 스터드일때 (5장 이상)
-            joinDealer()
-            var tempArr = [[Card]]()
-            for index in 0...players.members {
-                let temp = dealer.distributeCard(studNum: stud.rawValue)
-                tempArr.append(temp)
-                print("\(realPlayers[index]) \(tempArr[index])")
-            }
+        
+        action.inputPockerStud = { pokerStud in
+            self.pokerStud = pokerStud
+            self.resetGame()
         }
     }
 }
 
-extension PockerGame {
+//MARK: - Business Logic
+extension PokerGame {
     
-    enum Stud:Int {
-        case five = 5
-        case seven = 7
+    func resetGame() {
+        pokerPlayers.removeAllPlayer()
+        pokerPlayers.addPlayers(players: self.makeNewPlayers(count: playerCount))
+        pokerPlayers.addPlayer(player: dealer)
+        state.resetPockerBoard(pokerStud, pokerPlayers.names)
     }
     
+    private func play() {
+        pokerPlayers.removeAllCard()
+        dealer.cardShuffle()
+        
+        if dealer.hasRemainCardCount(stud: pokerStud, playerCount: pokerPlayers.count) == false {
+            self.state.finishPocker()
+            return
+        }
+        pokerPlayers.cardDistribution(dealer: dealer, pokerStud: pokerStud, addEventHandler: self.state.givePlayerCard, finishEventHandler: { self.state.pokerWinner(self.pokerPlayers.getWinner())
+        })
+    }
+    
+    private func makeNewPlayers(count: PokerPlayers.Count) -> [Participant] {
+        var nameData = ["Shingha", "Bibi", "Alex", "Rosa", "Chez",
+                        "Ocean", "Jason", "Dale", "Kai", "Jee",
+                        "Mase", "Sol", "Ebony", "Gucci", "Zed",
+                        "Beck", "Eddy", "Selina", "Pigbag", "Chole"]
+        var players = [Participant]()
+        while players.count != count.value {
+            let randomIndex = Int.random(in: 0..<nameData.count)
+            let name = nameData.remove(at: randomIndex)
+            if name.count < 2 || name.count > 5 { continue }
+            players.append(Participant(name: name))
+        }
+        return players
+    }
+}
+
+//MARK: - Parameter 선언 부
+extension PokerGame {
+    
+    enum Constants {
+        static let defaultPockerStud = PokerGame.Stud.fiveCard
+    }
+    
+    struct Action {
+        var pokerPlay: () -> Void = { }
+        var pokerReset: () -> Void = { }
+        var inputPockerStud: (Stud) -> Void = { _ in }
+        var inputPlayerCount: (PokerPlayers.Count) -> Void = { _ in }
+    }
+    
+    struct State {
+        var resetPockerBoard: (Stud, [String]) -> Void = { _, _ in }
+        var givePlayerCard: (Int, Int, Card) -> Void = { _, _, _ in }
+        var pokerWinner: (String?) -> Void = { _ in }
+        var finishPocker: () -> Void = { }
+    }
+}
+
+//MARK: - Poker Stud 집합 부
+extension PokerGame {
+    
+    enum Stud:CaseIterable {
+        case sevenCard
+        case fiveCard
+        
+        var cardCount: Int {
+            switch self {
+            case .sevenCard: return 7
+            case .fiveCard: return 5
+            }
+        }
+        
+        var index: Int {
+            switch self {
+            case .sevenCard: return 0
+            case .fiveCard: return 1
+            }
+        }
+        
+        func cardDistribution(playerCount: PokerPlayers.Count,
+                              loopEvent: @escaping (_ playerIndex: Int, _ cardIndex: Int) -> Void,
+                              finishEvennt: @escaping () -> Void) {
+            let maxLoopCount = cardCount * playerCount.value
+            let speed = 0.3
+            var loopCount = 0
+            
+            Timer.scheduledTimer(withTimeInterval: speed, repeats: true) { timer in
+                let cardIndex = loopCount / playerCount.value
+                let playerIndex = loopCount % playerCount.value
+                
+                loopEvent(playerIndex, cardIndex)
+                loopCount += 1
+                
+                if maxLoopCount == loopCount {
+                    finishEvennt()
+                    timer.invalidate()
+                }
+            }
+        }
+    }
 }
